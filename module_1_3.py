0f8173bd3c4730ecdf1416cf9bcad59bf80ff32f
Vitaliy23245 <Vitakup202@mail.ru>
2024-09-02 20:33:06 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name='Виталий'
 print('Name:'+(name))
 age=21
 print('Age:'+str(age))
-new_age= age+1
-print('New Age:'+str(new_age))
+age=age+1
+print('New Age:'+str(age))
 is_student = True
 print('Is student:', is_student)
